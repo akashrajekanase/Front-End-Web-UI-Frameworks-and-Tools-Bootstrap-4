9f8e11621bdb3af20feefe1aa8da3c44b012d1c5
Daniel Johnson <danny_boyx@hotmail.co.uk>
2018-09-24 15:54:55 +0100

    Configuring Angular

--- javascriptFrameworkAngular/conFusion/src/app/app.module.ts
+++ javascriptFrameworkAngular/conFusion/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
@@ -17,9 +17,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    //BrowserAnimationsModule,
-    //MaterialModule,
-    ///FlexLayoutModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
