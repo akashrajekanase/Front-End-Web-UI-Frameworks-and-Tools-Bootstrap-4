0bb93aebb1caa328638ba294d081f35b32689aae
Daniel Johnson <danny_boyx@hotmail.co.uk>
2018-09-24 17:59:47 +0100

    Components Part 2

--- javascriptFrameworkAngular/conFusion/src/app/app.module.ts
+++ javascriptFrameworkAngular/conFusion/src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
@@ -20,8 +25,13 @@ import { MenuComponent } from './menu/menu.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    
     MatToolbarModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    
     FlexLayoutModule
   ],
   providers: [],
