ebb412db07269d469b01cfb71edb123fe036bf6d
Daniel Johnson <danny_boyx@hotmail.co.uk>
2018-09-26 15:06:45 +0100

    Reactive Forms Part 1

--- javascriptFrameworkAngular/conFusion/src/app/app.module.ts
+++ javascriptFrameworkAngular/conFusion/src/app/app.module.ts
@@ -13,8 +13,11 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms'; 
 
+import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
@@ -63,8 +66,11 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
 
-    FormsModule, 
+    FormsModule,
+    ReactiveFormsModule,
     
     FlexLayoutModule
   ],
