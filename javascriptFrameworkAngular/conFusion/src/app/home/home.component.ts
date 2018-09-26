09dfdc246e2b72fbf21c86be7d5eac94e3d0b36b
Daniel Johnson <danny_boyx@hotmail.co.uk>
2018-09-26 18:27:40 +0100

    RxJs Part 1

--- javascriptFrameworkAngular/conFusion/src/app/home/home.component.ts
+++ javascriptFrameworkAngular/conFusion/src/app/home/home.component.ts
@@ -18,20 +18,14 @@ export class HomeComponent implements OnInit {
   promotion: Promotion;
   leader: Leader;
 
-  constructor(private dishService: DishService, 
+  constructor(private dishService: DishService,
     private promotionService: PromotionService,
-    private leaderService: LeaderService, 
-    ) { }
+    private leaderService: LeaderService,
+  ) { }
 
   ngOnInit() {
-    this.dishService.getFeaturedDish()
-      .then(dish => this.dish = dish);
-    this.promotionService.getFeaturedPromotion()
-      .then(promotion => this.promotion = promotion);
-    this.leaderService.getFeaturedLeader()
-      .then(leader => this.leader = leader);
-
-    
+    this.dishService.getFeaturedDish().subscribe(dish => this.dish = dish);
+    this.promotionService.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion);
+    this.leaderService.getFeaturedLeader().subscribe(leader => this.leader = leader);
   }
-
 }
