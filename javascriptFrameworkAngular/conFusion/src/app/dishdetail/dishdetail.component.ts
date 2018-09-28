8a7c9c7ed3998193a2eb9ac2640527bf4934da3d
Daniel Johnson <danny_boyx@hotmail.co.uk>
2018-09-28 15:13:59 +0100

    get ready for last assignment

--- javascriptFrameworkAngular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ javascriptFrameworkAngular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,9 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/comment';
 
+import { visibility, flyInOut, expand } from '../animations/app.animation';
+
+
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
@@ -10,13 +13,22 @@ import { switchMap } from 'rxjs/operators';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+  animations: [
+    flyInOut(),
+    visibility(),
+    expand()
+  ],
 })
 export class DishdetailComponent implements OnInit {
+  visibility = 'shown';
   commentForm: FormGroup;
   comment: Comment
   @ViewChild('fform') commentFormDirective;
@@ -62,10 +74,9 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params
-      .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
-        errmess => this.errMess = <any>errmess );
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+      errmess => this.errMess = <any>errmess);
   }
 
   goBack(): void {
